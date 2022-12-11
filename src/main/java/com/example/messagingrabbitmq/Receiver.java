9e9712f1987679f66c408830691d91b22ef594db
Vlad <luckyvalenok1@gmail.com>
2022-12-11 13:44:29 +0500

    Done

--- src/main/java/com/example/messagingrabbitmq/Receiver.java
+++ src/main/java/com/example/messagingrabbitmq/Receiver.java
@@ -1,20 +1,21 @@
 package com.example.messagingrabbitmq;
 
-import java.util.concurrent.CountDownLatch;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
 
 @Component
 public class Receiver {
-
-	private CountDownLatch latch = new CountDownLatch(1);
-
-	public void receiveMessage(String message) {
-		System.out.println("Received <" + message + ">");
-		latch.countDown();
-	}
-
-	public CountDownLatch getLatch() {
-		return latch;
-	}
-
+    
+    private final CountDownLatch latch = new CountDownLatch(1);
+    
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+    
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+    
 }
--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,27 +1,30 @@
 package com.example.messagingrabbitmq;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class Runner implements CommandLineRunner {
-
-	private final RabbitTemplate rabbitTemplate;
-	private final Receiver receiver;
-
-	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-		this.receiver = receiver;
-		this.rabbitTemplate = rabbitTemplate;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-	}
-
+    
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+    
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner stdin = new Scanner(System.in);
+        while (true) {
+            rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.exchangeName, MessagingRabbitmqApplication.queueName, stdin.nextLine());
+            receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        }
+    }
+    
 }
